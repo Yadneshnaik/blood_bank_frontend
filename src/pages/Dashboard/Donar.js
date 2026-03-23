34cc3f61d74794679e3a20934205eb837b7d26ec
Yadnesh Naik <naikyadnesh9@gmail.com>
2026-03-23 07:41:16 +0530

    init

--- src/pages/Dashboard/Donar.js
+++ src/pages/Dashboard/Donar.js
@@ -5,13 +5,21 @@ import moment from "moment";
 
 const Donar = () => {
   const [data, setData] = useState([]);
-  //find donar records
+  const [editingDonor, setEditingDonor] = useState(null);
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+
+  // GET DONORS
   const getDonars = async () => {
     try {
-      const { data } = await API.get("/inventory/get-donars");
-      //   console.log(data);
-      if (data?.success) {
-        setData(data?.donars);
+      const res = await API.get("/inventory/get-donars");
+
+      if (res.data.success) {
+        setData(res.data.donars);
       }
     } catch (error) {
       console.log(error);
@@ -22,28 +30,36 @@ const Donar = () => {
     getDonars();
   }, []);
 
+
   return (
     <Layout>
-      <table className="table ">
-        <thead>
-          <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">Phone</th>
-            <th scope="col">Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data?.map((record) => (
-            <tr key={record._id}>
-              <td>{record.name || record.organisationName + " (ORG)"}</td>
-              <td>{record.email}</td>
-              <td>{record.phone}</td>
-              <td>{moment(record.createdAt).format("DD/MM/YYYY hh:mm A")}</td>
+      <div className="container">
+
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th>Date</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+
+          <tbody>
+            {data?.map((record) => (
+              <tr key={record._id}>
+                <td>{record.name || record.organisationName + " (ORG)"}</td>
+                <td>{record.email}</td>
+                <td>{record.phone}</td>
+                <td>
+                  {moment(record.createdAt).format("DD/MM/YYYY hh:mm A")}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+      </div>
     </Layout>
   );
 };
